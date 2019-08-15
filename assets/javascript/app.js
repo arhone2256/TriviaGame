7da3eec09d55758fddf49548a448cebf572556d7
alex rhone <arhone4259@gmail.com>
2019-08-15 19:45:08 -0400

    start?

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    
+    // Questions & Answers
+    var options = [
+        {
+            question: "What is the largest bone in the human body?" ,
+            choice: ["Tibula" , "Femur" , "rib" , "nugget"],
+            answer:  1
+        },
+        {
+            question:" Who was the greek god of dreams?",
+            choice: [ " Morpheus "," Kratos "," Anubis ","Sleepy"],
+            answer: 0
+        },
+        {
+            question:" Which pop album is the best selling of all time?",
+            choice: ["reputation by Taylor Swift","Back in Black ACDC"," Thriller by MJ","Abbey road by the Beatles"],
+            answer: 2
+        },
+        {
+            question:" Who was the first american to go into space? ",
+            choice: ["Alex Rhone","Neil Armstrong","Alan Shephard","Keith Kovac"],
+            answer: 2
+        },
+        {
+            question:"What is the name of the fairy in Peter Pan?",
+            choice: ["Tinker Bell","Donald Trump","Mittens","Rosco"],
+            answer:0
+        }];
+
+
+    });
